6fa996d86682ae6d701b974ba53e3aba8788fb82
Arthur A. Selimov <am35a.piston@gmail.com>
2021-09-02 22:42:47 +0300

    create each for items

--- app/src/lib/layout/laborer/aside/items/Item/Item.svelte
+++ app/src/lib/layout/laborer/aside/items/Item/Item.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
     import Icon from '../../../../../../components/Icon.svelte'
-
+    export let active: boolean = false
+    export let icon: string = 'ghost'
+    export let title: string = undefined
+    export let segment : string = undefined
 </script>
 
-<div class="item">
+<div on:click={() => active = !active} 
+    class:item = {true}
+    class:active = {active}
+    {...$$restProps}
+>
     <div class="hover"></div>
-    <div class="active"></div>
+    <div class="status"></div>
     <div class="icon">
-        <Icon name="home" size="sm"/>
+        <Icon name={icon} size="lg"/>
     </div>
-    <div class="title">home</div>
+    <div class="title">{title}</div>
 </div>
 
 <style lang="sass">
@@ -17,16 +24,42 @@
         display: grid
         height: var(--layout-aside-item-height)
         color: var(--positive-color)
+        cursor: pointer
         .hover
             grid-column: 1/-1
             grid-row: 1/-1
-        .active
+            transition: background-color .25s
+        .status
             grid-column: 1/-1
             grid-row: 1/-1
+            margin: auto auto auto .25rem
+            width: .25rem
+            height: calc(100% - 0.5rem)
+            border-radius: .125rem
+            background-color: transparent
+            transition: height .25s, background-color .25s .125s        
+        &.active
+            .status
+                margin: auto auto auto .25rem
+                width: .25rem
+                height: 0.25rem
+                border-radius: .125rem
+                background-color: var(--positive-color)
+                transition: height .25s, background-color .25s
         .icon
             grid-column: 1/-1
             grid-row: 1/-1
+            display: grid
+            width: calc(var(--key-size) * 3)
+            justify-items: center
+            align-items: center
         .title
             grid-column: 1/-1
             grid-row: 1/-1
+            display: grid
+            margin: auto auto auto calc(var(--key-size) * 3)
+            text-transform: uppercase
+        &:hover
+            .hover
+                background-color: var(--positive-color-25)
 </style>
--- app/src/lib/layout/laborer/aside/items/Items.svelte
+++ app/src/lib/layout/laborer/aside/items/Items.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
     import Item from './item/Item.svelte'
+
+    export let segment = 'home' // move to store
+    const items = [
+        {
+            id: 1,
+            icon: 'home',
+            title: 'Home page',
+            segment: 'home'
+        },		
+        {
+            id: 2,
+            icon: 'dashboard',
+            title: 'Dashboards',
+            segment: 'dashboard'
+        },
+        {
+            id: 3,
+            icon: 'help',
+            title: 'Get help',
+            segment: 'help'
+        }
+    ]
+
 </script>
 
 <div class="items">
-    <Item>
-
-    </Item>
+    {#each items as item (item.id)}
+        <!-- <Item icon={item.icon} title={item.title} active={segment === item.segment} segment={segment} /> -->
+        <Item {...item} active={segment === item.segment} segment={segment} />
+    {/each}
 </div>
 
 <style lang="sass">
