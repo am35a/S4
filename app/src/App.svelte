bdcbfd9e6fd081f3c2bc44a53951a5b9664dbb04
Arthur A. Selimov <am35a.piston@gmail.com>
2021-09-22 20:49:07 +0300

    test mockend json data

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { onMount } from 'svelte'
     import { user } from './store/store'
 
     import Head from './lib/DocumentHead.svelte'
@@ -7,6 +8,20 @@
     import LayoutSignIn from './lib/layout/signin/Index.svelte'
     import LayoutLaborer from './lib/layout/laborer/Laborer.svelte'
     
+    let appData: any
+
+	onMount(async () => {
+        // https://learn.javascript.ru/fetch
+
+           const res = await fetch('https://mockend.com/am35a/mockend/posts')
+           appData = await res.json()
+           console.log(appData)
+
+        // if ($user.settings.autoSignIn) {
+        //     console.log(`autoSignIn = ${$user.settings.autoSignIn}`)
+        //     route.goto('list')
+        // }
+	})    
 </script>
 
 <Head />
