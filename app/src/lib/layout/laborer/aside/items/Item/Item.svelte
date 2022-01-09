2b9c25fe1fcb2e632eb014074629c81afcbaeb81
Arthur A. Selimov <am35a.piston@gmail.com>
2022-01-09 20:56:36 +0300

    rebuild Item component

--- app/src/lib/layout/laborer/aside/items/Item/Item.svelte
+++ app/src/lib/layout/laborer/aside/items/Item/Item.svelte
@@ -2,7 +2,7 @@
     import { onMount } from 'svelte'
 
     import { router, pathToArray, path } from 'svelte-micro'
-    import { asideExpand, appSegment } from 'src/store/store'    
+    import { asideExpand, appSegment } from 'src/store/store'
 
     import Icon from 'src/components/Icon.svelte'
     // import Subitem from './subitem/Subitem.svelte'
@@ -12,13 +12,20 @@
     export let title: string = undefined
     export let action: any = undefined
     export let segment: string = undefined
-    // export let items: any = undefined
+    export let items: any = undefined
 
     function onTitleClick() {
         router.push(segment)
         $appSegment = segment
         $asideExpand = false
     }
+
+    function onActionClick(event: any) {
+        console.log('action')
+        // event.Blur()
+        // event.blur()
+    }
+
 
     onMount(async () => {
         // let loadPath: string
@@ -31,40 +38,102 @@
 
 </script>
 
+<!-- class:active = {active} -->
 <div 
     class:item = {true}
-    class:active = {active}
+    class:has-action = {action}
+    class:has-submenu = {items}
     {...$$restProps}
 >
+    <button
+        class="title" on:click={onTitleClick} disabled={active}>{title}</button>
+    {#if action}
+        <button class="action" on:click={onActionClick} tabindex={-1}>
+            <Icon name={action.icon}/>
+        </button>
+    {/if}
     <div class="backdrop"></div>
     <div class="status"></div>
     <div class="icon">
         <Icon name={icon} size="lg"/>
     </div>
-    {#if action}
-        <div class="action">
-            <Icon name={action.icon}/>
-        </div>
-    {/if}
-    <button on:click={onTitleClick} class="title">{title}</button>
+    <!-- <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+    </div> -->
 </div>
-<!-- <Subitem /> -->
 
 <style lang="sass">
     .item
         display: grid
-        // height: var(--layout-aside-item-height)
-        color: var(--aside-item-color)
-        // transition: color 0.25s
-        transition: all 0.25s
-
+        &.has-action,
+        &.has-submenu
+            .title
+                padding-right: 3rem
+        .title
+            grid-column: 1/-1
+            grid-row: 1/-1
+            display: grid
+            height: var(--layout-aside-item-height)
+            justify-items: start
+            align-items: center
+            padding-left: calc(var(--key-size) * 3)
+            padding-right: 1rem
+            max-width: 100%
+            color: var(--aside-item-title-color)
+            cursor: pointer
+            text-transform: capitalize
+            user-select: none
+            z-index: 1
+            &:not(:disabled)
+                &:focus,
+                &:hover
+                    color: var(--positive-color)
+                    ~ .backdrop
+                        margin: calc(var(--key-size) * .25)
+                        border-radius: calc(var(--key-size) * .25)
+                        background: var(--aside-item-backdrop--hover-bg, var(--positive-color-10))
+                    ~ .icon
+                        color: var(--positive-color)
+            &:disabled
+                cursor: default !important
+                pointer-events: none
+                color: var(--aside-item-active-color, var(--positive-color))
+                ~ .status
+                    width: .375rem
+                    background: var(--aside-item-active-status-bg, var(--positive-color))
+                    transition: width .25s .125s, background .25s .125s
+        .action
+            grid-column: 1/-1
+            grid-row: 1/-1
+            display: grid
+            margin: 0 0 0 auto
+            width: calc(var(--key-size) * 3)
+            height: 100%
+            justify-items: center
+            align-items: center
+            color: var(--aside-item-action-color)
+            cursor: pointer
+            z-index: 2
+            :global(.ico)
+                transform: scale(.5)
+                transition: transform .25s
+            // &:focus,
+            &:hover
+                color: var(--positive-color)
+                :global(.ico)
+                    transform: scale(.625)
+                ~ .backdrop
+                    margin: calc(var(--key-size) * .5)
+                    margin-left: calc(100% - calc(var(--key-size) * 3 - calc(var(--key-size) * .5)))
+                    border-radius: calc(var(--key-size) * .25)
+                    background: var(--aside-item-backdrop--hover-bg, var(--positive-color-10))
         .backdrop
             grid-column: 1/-1
             grid-row: 1/-1
             margin: 0
             border-radius: 0
             background: var(--aside-item-backdrop-bg)
-            transition: background .25s, margin .25s, border-radius .25s
+            transition: background .25s, margin .25s, border-radius .25s,
         .status
             grid-column: 1/-1
             grid-row: 1/-1
@@ -85,57 +154,4 @@
             align-items: center
             color: var(--aside-item-icon-color)
             z-index: 0
-        .action
-            grid-column: 1/-1
-            grid-row: 1/-1
-            display: grid
-            margin: 0 0 0 auto
-            width: calc(var(--key-size) * 3)
-            height: 100%
-            justify-items: center
-            align-items: center
-            color: var(--aside-item-action-color)
-            cursor: pointer
-            z-index: 2
-            :global(.ico)
-                transform: scale(.5)
-                transition: transform .25s
-            &:hover
-                :global(.ico)
-                    transform: scale(.75)
-            + .title
-                padding-right: 3rem
-        .title
-            grid-column: 1/-1
-            grid-row: 1/-1
-            display: grid
-            height: var(--layout-aside-item-height)
-            justify-items: start
-            align-items: center
-            padding-left: calc(var(--key-size) * 3)
-            padding-right: 1rem
-            max-width: 100%
-            color: var(--aside-item-title-color)
-            cursor: pointer
-            text-transform: capitalize
-            user-select: none
-            z-index: 1
-        &:hover
-            color: var(--positive-color)
-            .backdrop
-                margin: calc(var(--key-size) * .25)
-                border-radius: calc(var(--key-size) * .25)
-                background: var(--aside-item-backdrop--hover-bg, var(--positive-color-10))
-        &.active
-            color: var(--aside-item-active-color, var(--positive-color))
-            .status
-                width: .375rem
-                background: var(--aside-item-active-status-bg, var(--positive-color))
-                transition: width .25s .125s, background .25s .125s
-            .title
-                cursor: default
-                pointer-events: none
-                color: var(--aside-item-active-title-color)
-                
-                
 </style>
