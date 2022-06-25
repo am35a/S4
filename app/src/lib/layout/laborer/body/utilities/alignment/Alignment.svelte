5f56813e9246f39731a828e341a7c3746a68f20e
Arthur A. Selimov <am35a.piston@gmail.com>
2022-06-25 23:31:26 +0300

    fix small and line height

--- app/src/lib/layout/laborer/body/utilities/alignment/Alignment.svelte
+++ app/src/lib/layout/laborer/body/utilities/alignment/Alignment.svelte
@@ -9,20 +9,24 @@
         <div class="h3" aria-level={3} role="heading">Vertical alignment</div>
         <p>...</p>
         <div class="d-grid g-2 overflow-x-auto">
-            <p class="lh-lg">
+            <div class="col-span-full row-span-full my-auto border-bottom border-secondary"><!-- base line indicator--></div>
+            <div class="col-span-full row-span-full lh-lg">
                 <span class="bg-positive px-1 rounded align-baseline">baseline</span>
                 <span class="bg-positive px-1 rounded align-top">top</span>
                 <span class="bg-positive px-1 rounded align-middle">middle</span>
                 <span class="bg-positive px-1 rounded align-bottom">bottom</span>
                 <span class="bg-positive px-1 rounded align-text-bottom">text-bottom</span>
                 <span class="bg-positive px-1 rounded align-text-top">text-top</span>
-            </p>
-
-
+            </div>
         </div>
         <Precode>
             {
-`...`
+`<span class="align-baseline">baseline</span>
+<span class="align-top">top</span>
+<span class="align-middle">middle</span>
+<span class="align-bottom">bottom</span>
+<span class="align-text-bottom">text-bottom</span>
+<span class="align-text-top">text-top</span>`
             }
         </Precode>
     </div>
--- app/src/lib/layout/laborer/body/utilities/typography/Typography.svelte
+++ app/src/lib/layout/laborer/body/utilities/typography/Typography.svelte
@@ -1,3 +1,8 @@
 <section>
     <div class="h2" aria-level={2} role="heading">Typography</div>
+
+    <small>one small</small>
+    <small><small>bouble small</small></small>
+    <small><small><small>triple small</small></small></small>
+
 </section>
