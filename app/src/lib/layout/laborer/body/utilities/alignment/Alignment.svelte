42a343667ebea008202e02cd1df5900837c0b2fd
Arthur A. Selimov <am35a.piston@gmail.com>
2022-06-25 15:04:27 +0300

    playing with line-height

--- app/src/lib/layout/laborer/body/utilities/alignment/Alignment.svelte
+++ app/src/lib/layout/laborer/body/utilities/alignment/Alignment.svelte
@@ -1,3 +1,29 @@
+<script lang="ts">
+    import Precode from "src/components/Precode.svelte"
+
+</script>
+
 <section>
     <div class="h2" aria-level={2} role="heading">Alignment</div>
+    <div class="d-grid">
+        <div class="h3" aria-level={3} role="heading">Vertical alignment</div>
+        <p>...</p>
+        <div class="d-grid g-2 overflow-x-auto">
+            <p class="lh-lg">
+                <span class="bg-positive px-1 rounded align-baseline">baseline</span>
+                <span class="bg-positive px-1 rounded align-top">top</span>
+                <span class="bg-positive px-1 rounded align-middle">middle</span>
+                <span class="bg-positive px-1 rounded align-bottom">bottom</span>
+                <span class="bg-positive px-1 rounded align-text-bottom">text-bottom</span>
+                <span class="bg-positive px-1 rounded align-text-top">text-top</span>
+            </p>
+
+
+        </div>
+        <Precode>
+            {
+`...`
+            }
+        </Precode>
+    </div>
 </section>
