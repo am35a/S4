ad3f368ab457a998ba3033fcef6070d4acd1a350
Arthur A. Selimov <am35a.piston@gmail.com>
2022-07-05 06:43:23 +0300

    added --cols-repeat val for cols-x

--- app/src/lib/layout/laborer/body/utilities/grid/Grid.svelte
+++ app/src/lib/layout/laborer/body/utilities/grid/Grid.svelte
@@ -1,3 +1,20 @@
 <section>
     <div class="h2" aria-level={2} role="heading">Grid</div>
+    <!-- <div class="d-grid cols-5 rows-3 g-4">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+        <div>6</div>
+        <div>7</div>
+        <div>Eight</div>
+        <div>9</div>
+        <div>10</div>
+        <div>11</div>
+        <div>12</div>
+        <div>13</div>
+        <div>14</div>
+        <div>15</div>
+    </div> -->
 </section>
