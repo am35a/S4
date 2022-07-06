39cfc4273897632119ca4df8ec269b97f76d208f
Arthur A. Selimov <am35a.piston@gmail.com>
2022-07-06 22:45:19 +0300

    fit and fill text example

--- app/src/lib/layout/laborer/body/utilities/grid/Grid.svelte
+++ app/src/lib/layout/laborer/body/utilities/grid/Grid.svelte
@@ -1,20 +1,68 @@
 <section>
     <div class="h2" aria-level={2} role="heading">Grid</div>
-    <!-- <div class="d-grid cols-5 rows-3 g-4">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>Eight</div>
-        <div>9</div>
-        <div>10</div>
-        <div>11</div>
-        <div>12</div>
-        <div>13</div>
-        <div>14</div>
-        <div>15</div>
-    </div> -->
+    <div class="bg-error d-grid cols-5 rows-3 g-2" style="--cols-repeat: auto-fill; --cols-min: 5em; --cols-max: auto;">
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">width more then 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+    </div>
+    <div class="my-8"></div>
+    <div class="bg-error d-grid cols-5 rows-3 g-2" style="--cols-repeat: auto-fit; --cols-min: 5em; --cols-max: auto;">
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">width more then 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+        <div>
+            <div class="bg-warning">less 10em</div>
+        </div>
+    </div>
 </section>
