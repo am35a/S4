2aa1c19998fa1105b4c262b12ca4a93f1bde0f8c
Arthur A. Selimov <am35a.piston@gmail.com>
2022-09-01 07:10:35 +0300

    package update + play with overflow

--- app/src/lib/layout/laborer/body/utilities/overflow/Overflow.svelte
+++ app/src/lib/layout/laborer/body/utilities/overflow/Overflow.svelte
@@ -1,14 +1,29 @@
 <script lang="ts">
+    import Button from 'component/Button.svelte'
     import Precode from 'component/Precode.svelte'
     import Details from 'component/Details.svelte'
+
+    let overflowPropertyArr: string[] = [ 'auto', 'hidden', 'visible', 'scroll' ]
+    let overflowProperty: string = overflowPropertyArr[0]
 </script>
 <section>
-    <h2>Overflow</h2>
+    <h2 class="overflow">Overflow</h2>
     <p>
         ...
     </p>
-    <div class="d-grid g-2 p-2 bgc-positive br">
-        ...
+    <div class="d-grid g-3 mb-4 p-3 bgc-positive br">
+        <div class="d-flex g-2">
+            {#each overflowPropertyArr as values }
+                <Button
+                    class="btn-secondary {overflowProperty === values ? 'active' : ''}"
+                    on:click = {() => overflowProperty = values}
+                    disabled = {overflowProperty === values}
+                >o-{values}</Button>
+            {/each}
+        </div>
+        <div class="o-{overflowProperty} mx-auto bgc-negative-10 p-2" style="width: 240rem; height: 160rem;">
+            The Delta S4 competed in the World Rally Championship in 1985 and 1986, until Group B class was disbanded and the cars were eventually banned from competition completely by European sanctioning body FIA. The car replaced, and was an evolution of the 037.
+        </div>
     </div>
     <Details>
         <svelte:fragment slot="title">
