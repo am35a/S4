4cf546ff4197b11e49f9fb4c27c5ebf446e8ab1a
Arthur A. Selimov <am35a.piston@gmail.com>
2022-09-03 22:43:54 +0300

    position added

--- app/src/lib/layout/laborer/body/utilities/position/Position.svelte
+++ app/src/lib/layout/laborer/body/utilities/position/Position.svelte
@@ -1,3 +1,24 @@
-<section>
-    <h2>Position</h2>
-</section>
+<script lang="ts">
+    import Precode from 'component/Precode.svelte'
+    import Details from 'component/Details.svelte'
+</script>
+
+<article>
+    <h2 id="position">Position</h2>
+    <p>
+        For the location of the element in the document area determine the classes: <b>p-static</b>, <b>p-relative</b>, <b>p-absolute</b>, <b>p-fixed</b> and <b>p-sticky</b>.
+    </p>
+    <!-- <Details>
+        <svelte:fragment slot="title">
+            Code example
+        </svelte:fragment>
+        <svelte:fragment slot="body">
+            <Precode class="br-top-0">
+                {
+`...
+`
+                }
+            </Precode>
+        </svelte:fragment>
+    </Details> -->
+</article>
