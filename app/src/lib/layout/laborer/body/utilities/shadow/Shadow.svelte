545a0e9d10ea71396901fb4a04427263bfabcf84
Arthur A. Selimov <am35a.piston@gmail.com>
2022-09-04 07:34:39 +0300

    fix shadow

--- app/src/lib/layout/laborer/body/utilities/shadow/Shadow.svelte
+++ app/src/lib/layout/laborer/body/utilities/shadow/Shadow.svelte
@@ -1,3 +1,32 @@
-<section>
-    <div class="h2" aria-level={2} role="heading">Shadow</div>
-</section>
+<article>
+    <h2>Shadow</h2>
+    <p>
+        ...
+    </p>
+    <div class="d-grid g-3 cols-2 mb-4 p-3 bgc-positive br">
+        <div class="bgc-positive p-2 rounded s-xxs">
+            s-xxs
+        </div>
+        <div class="bgc-positive p-2 rounded s-xs">
+            s-xs
+        </div>
+        <div class="bgc-positive p-2 rounded s-sm">
+            s-sm
+        </div>
+        <div class="bgc-positive p-2 rounded s-md">
+            s-md
+        </div>
+        <div class="bgc-positive p-2 rounded s-lg">
+            s-lg
+        </div>
+        <div class="bgc-positive p-2 rounded s-xl">
+            s-xl
+        </div>
+        <div class="bgc-positive p-2 rounded s-xxl">
+            s-xxl
+        </div>
+        <div class="bgc-positive p-2 rounded s-xxxl">
+            s-xxxl
+        </div>
+    </div>
+</article>
