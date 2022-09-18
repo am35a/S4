d8111bfa264ff786a661f82bf7a03906535fed35
Arthur A. Selimov <am35a.piston@gmail.com>
2022-09-18 10:09:28 +0300

    fix float and add tmp for border color

--- app/src/lib/layout/laborer/body/utilities/border/Border.svelte
+++ app/src/lib/layout/laborer/body/utilities/border/Border.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
     import SectionRadius from './radius/Radius.svelte'
     import SectionWidth from './width/Width.svelte'
+    import SectionColor from './color/Color.svelte'
 </script>
 
 <article>
     <h2 id="border">Border</h2>
     <SectionRadius />
     <SectionWidth />
+    <SectionColor />
 </article>
--- app/src/lib/layout/laborer/body/utilities/float/Float.svelte
+++ app/src/lib/layout/laborer/body/utilities/float/Float.svelte
@@ -8,13 +8,24 @@
     <p>
         Use <b>f-...</b> classes to floats any element across any breakpoint.
     </p>
+    <div class="alert alert-warning js-start">
+        * Pay attention the <b>f-start</b> and <b>f-start</b> classes do not support by invalid browsers like Chrome or Safari.
+    </div>
     <div class="d-grid g-3 mb-4 p-3 bgc-positive br">
         <div class="bgc-negative-10 p-2">
-            <div class="f-start bgc-negative-10 p-2">f-start</div>
+            <div class="f-left bgc-negative-10 p-2">f-left</div>
+            Unde quae consequatur optio, saepe, harum modi nobis excepturi cum ducimus minima architecto tenetur laudantium quaerat rem fuga nihil nemo distinctio beatae minus, rerum voluptate vel facere. Corporis, blanditiis inventore!
+        </div>
+        <div class="bgc-negative-10 p-2">
+            <div class="f-right bgc-negative-10 p-2">f-right</div>
+            Dicta modi eaque, commodi delectus sint enim perspiciatis, cupiditate inventore sequi beatae pariatur at earum perferendis eius doloribus blanditiis minima temporibus voluptates. Possimus assumenda quasi, quaerat id molestiae nihil sint.
+        </div>
+        <div class="bgc-negative-10 p-2">
+            <div class="f-start bgc-negative-10 p-2">f-start *</div>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam sequi provident dolores impedit, excepturi eaque dolore, sint, quasi qui voluptatum culpa adipisci ad non. Libero perspiciatis illo error porro ea?
         </div>
         <div class="bgc-negative-10 p-2">
-            <div class="f-end bgc-negative-10 p-2">f-end</div>
+            <div class="f-end bgc-negative-10 p-2">f-end *</div>
             Voluptates recusandae libero nostrum molestiae commodi minus doloribus, dignissimos ratione sint atque tenetur est quam, magni quae consequatur deserunt optio pariatur architecto! Non eaque animi ab qui officia nam vel.
         </div>
         <div class="bgc-negative-10 p-2">
