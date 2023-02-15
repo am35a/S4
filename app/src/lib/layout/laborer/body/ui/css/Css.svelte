1fed95509896f9d25b39ded82e17065666b7eb97
Arthur A. Selimov <am35a.piston@gmail.com>
2023-02-15 20:49:03 +0300

    fix adaptive for grid example

--- app/src/lib/layout/laborer/body/ui/css/Css.svelte
+++ app/src/lib/layout/laborer/body/ui/css/Css.svelte
@@ -111,18 +111,25 @@
 
         <h3 id="grid">Grid by grid</h3>
         <div class="d-grid cols-12 g-4 p-4 bgc-positive br-lg">
-            <div class="bgc-negative-10 br px-2 py-1 col-span-1">one</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-11">eleven</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-2">two</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-10">ten</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-3">three</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-9">nine</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-4">four</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-8">eight</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-5">five</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-7">seven</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-6">six</div>
-            <div class="bgc-negative-10 br px-2 py-1 col-span-6">sex</div>
+            <p class="col-span-12">
+                Play your imagination without limits to be completely responsive now S4 Grid has everything you need! 
+            </p>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-1">one <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-11">eleven <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-2">two <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-10">ten <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-3">three <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-9">nine <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-4">four <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-8">eight <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-5">five <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-7">seven <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-6">six <span class="d-none p:d-inline-block">to twelve</span></div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden p:col-span-12 col-span-6">sex <span class="d-none p:d-inline-block">to twelve</span></div>
+        </div>
+        <div class="d-grid cols-12 g-4 p-4 bgc-positive br-lg">
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden col-span-2 col-start-3">two</div>
+            <div class="bgc-negative-10 br py-1 ta-center o-hidden col-span-6 col-start-7">six</div>
         </div>
     </section>
 </article>
