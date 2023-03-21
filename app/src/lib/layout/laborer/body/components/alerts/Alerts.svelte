4a84afd1ed14c9c7a6769db862a6e6ba1c60e829
Arthur A. Selimov <am35a.piston@gmail.com>
2023-03-21 03:05:12 +0100

    fixed outline for alerts

--- app/src/lib/layout/laborer/body/components/alerts/Alerts.svelte
+++ app/src/lib/layout/laborer/body/components/alerts/Alerts.svelte
@@ -5,7 +5,8 @@
     let alertsObj = {
         variantsObj: {
             Default: '' as string,
-            Alt: 'alt' as string
+            Alt: 'alt' as string,
+            Outline: 'outline' as string
         },
         colorsObj: {
             Primary: '-primary' as string,
@@ -36,6 +37,8 @@
                         return ''
         }
     }
+
+    let customClasses: string = ''
 </script>
 
 <article>
@@ -43,6 +46,7 @@
     <p>
         To add the close button for killing an alerts add the <b>alert-closable</b> class name.
     </p>
+
     <div class="d-grid g-5">
         <div class="d-grid g-4">
             <b>Variants</b>
@@ -72,17 +76,34 @@
                 {/each}
             </div>
         </div>
-        <div class="d-flex g-4 p-4 bgc-positive br">
-            <Alert class="{alertClass} alert-closable mb-0">
-                Click the close icon in the top right corner to destroy the block of alert message.
-            </Alert>
-        </div>        
+        <div class="d-grid g-4">
+            <b>Custom</b>
+            <input
+                id="customClasses"
+                bind:value={customClasses}
+                type="text"
+                placeholder="Type utility classes to customizing the component"
+            >
+        </div>
+        <div class="d-grid g-4">
+            <b>Preview</b>
+            <div class="d-flex g-4 p-4 bgc-positive br">
+                <Alert class="{alertClass} {customClasses} alert-closable mb-0">
+                    Click the close icon in the top right corner to destroy the block of alert message.
+                </Alert>
+            </div>        
+        </div>
         <div class="d-grid g-4">
             <div class="d-grid g-4">
                 <b>Svelte</b>
                 <Precode>
                     {
-`<Alert class="${alertClass} alert-closable">...</Alert>
+`// before using the component, import it
+// import Alert from 'component/Alert.svelte'
+
+<Alert class="${alertClass} alert-closable">
+    Alert component place.
+</Alert>
 `
                     }
                 </Precode>
