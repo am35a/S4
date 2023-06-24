fc0b06cb32043a3d2fe9b62cd8279003f76cc355
Arthur A. Selimov <am35a.piston@gmail.com>
2023-06-24 09:19:43 +0300

    ColorBox ready to oklch

--- app/src/lib/layout/laborer/body/colors/Colors.svelte
+++ app/src/lib/layout/laborer/body/colors/Colors.svelte
@@ -2,45 +2,82 @@
     import Alert from 'src/components/Alert.svelte'
     import ColorBox from './colorbox/ColorBox.svelte'
 
-    let colorPaletteObj = {
-        default: {
-            offset: 10,
-            positive: { h: 0, s: 0, l: 100 },
-            negative: { h: 0, s: 0, l: 0 }
-        },
-        accent: {
-            settings: {
-                offset: 15,
-                mute: .3
-            },
-            colors: {
-                primary: { h: 210, s: 90, l: 50 },
-                secondary: { h: 210, s: 10, l: 50 },
-                info: { h: 190, s: 90, l: 50 },
-                success: { h: 120, s: 50, l: 50 },
-                warning: { h: 40, s: 90, l: 50 },
-                error: { h: 0, s: 50, l: 50 }
-            }
-        }
+    // let colorPaletteObj = {
+    //     default: {
+    //         offset: 10,
+    //         positive: { h: 0, s: 0, l: 100 },
+    //         negative: { h: 0, s: 0, l: 0 }
+    //     },
+    //     accent: {
+    //         settings: {
+    //             offset: 15,
+    //             mute: .3
+    //         },
+    //         colors: {
+    //             primary: { h: 210, s: 90, l: 50 },
+    //             secondary: { h: 210, s: 10, l: 50 },
+    //             info: { h: 190, s: 90, l: 50 },
+    //             success: { h: 120, s: 50, l: 50 },
+    //             warning: { h: 40, s: 90, l: 50 },
+    //             error: { h: 0, s: 50, l: 50 }
+    //         }
+    //     }
+    // }
+    // const styleSheets = window.getComputedStyle(document.body)
+
+    let root = document.querySelector(':root')
+
+    function getVarValue(varName) {
+        // styleSheets.setProperty(varName, '#000')
+        // return styleSheets.getPropertyValue(varName)
+
+        // root.style.setProperty(varName, 'blue');
+        // return getComputedStyle(root).getPropertyValue(varName)
+
+        // return getComputedStyle(document.documentElement).getPropertyValue(varName) || 'empty :('
+        console.log(getComputedStyle(root).getPropertyValue('--lightness'))
+        // return getComputedStyle(document.body).getPropertyValue('--lightness')
+
     }
 </script>
 
+
 <h1>Colors</h1>
 
+<!-- {getVarValue('--lightness')} -->
+<!-- --lightness: {styleSheets.getPropertyValue('background-color')}
+{JSON.stringify(styleSheets)} -->
 
 
+<!-- --lightness: {JSON.stringify(getVarValue('--lightness'))} -->
+
+
+<!-- https://www.youtube.com/watch?v=HpZbIGFZlwE -->
+{getComputedStyle(document.querySelector(':root')).getPropertyValue('--lightness')}
+
+<!-- --lightness-offset: 15%
+--chroma: 0.2
+--alfa: 0.25
+
+--primary-hue: 255
+--secondary-hue: 255
+--info-hue: 295
+--success-hue: 145
+--warning-hue: 85
+--error-hue: 25 -->
 
 <div>
     Settings:
 </div>
 <div>
-    offset {colorPaletteObj.accent.settings.offset} mute {colorPaletteObj.accent.settings.mute}
+    <!-- offset {colorPaletteObj.accent.settings.offset} mute {colorPaletteObj.accent.settings.mute} -->
 </div>
 <div>
     Colors
 </div>
 <div class="d-flex fw-wrap g-2">
-    {#each Object.entries(colorPaletteObj.accent.colors) as [key, value]}
+    <ColorBox  l={60} c={0.2} h={255} offset={15} alfa={0.25}/>
+    <!-- {#each Object.entries(colorPaletteObj.accent.colors) as [key, value]}
         <div class="d-grid ji-center p-4 bgc-positive br f" style="--fg: 1; --fb: 0;">
             <div>
                 <div class="ta-center fw-600">{key}</div>
@@ -67,5 +104,5 @@
                 hsl({value.h} {value.s} {value.l} / {colorPaletteObj.accent.settings.mute})
             </div>
         </div>
-    {/each}
+    {/each} -->
 </div>
--- app/src/lib/layout/laborer/body/colors/colorbox/ColorBox.svelte
+++ app/src/lib/layout/laborer/body/colors/colorbox/ColorBox.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-    export let h: number = undefined
-    export let s: number = undefined
     export let l: number = undefined
+    export let c: number = undefined
+    export let h: number = undefined
     export let offset: number = undefined
-    export let mute: number = undefined
+    export let alfa: number = undefined
 </script>
 
 <svg
@@ -14,22 +14,22 @@
 >
     <path
         class="color-mute"
-        fill="hsl({h} {s}% {l}% / {mute})"
+        fill="oklch({l + offset}% {c} {h} / {alfa})"
         d="M 32 2 L 62 17 L 62 45 L 32 62 L 2 45 L 2 17 L 32 2 Z"
     />
     <path
         class="color-light"
-        fill="hsl({h} {s}% {l + offset}%)"
+        fill="oklch({l + offset}% {c} {h})"
         d="M 4 18 L 32 32 L 32 60 L 4 44 L 4 18 Z"
     />
     <path
         class="color-default"
-        fill="hsl({h} {s}% {l}%)"
+        fill="oklch({l}% {c} {h})"
         d="M 4 18 L 32 4 L 60 18 L 32 32 L 4 18 Z"
     />
     <path
         class="color-dark"
-        fill="hsl({h} {s}% {l - offset}%)"
+        fill="oklch({l - offset}% {c} {h})"
         d="M 32 32 L 60 18 L 60 44 L 32 60 L 32 32 Z"
     />
 </svg>
