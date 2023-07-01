37330aae747a055f9bd2893061317bb2c825dc5b
Arthur A. Selimov <am35a.piston@gmail.com>
2023-07-01 07:40:49 +0300

    fixed alert outline overview

--- app/src/lib/layout/laborer/body/ui/css/alerts/Alerts.svelte
+++ app/src/lib/layout/laborer/body/ui/css/alerts/Alerts.svelte
@@ -1,7 +1,7 @@
 <section class="d-grid g-5">
     <h3 id="alerts">Alerts</h3>
-    <div class="d-grid cols-2 p:cols-1 g-4">
-        <div class="d-grid p-4 g-4 bgc-positive br-lg">
+    <div class="d-flex fw-wrap g-4">
+        <div class="d-grid p-4 g-4 bgc-positive br-lg f" style="--fg: 1;">
             <div class="alert alert-primary">
                 Give importance with this simple alert.
             </div>
@@ -26,7 +26,7 @@
                 Error. Can`t connect to the platform.
             </div>
         </div>
-        <div class="d-grid p-4 g-4 bgc-positive br-lg">
+        <div class="d-grid p-4 g-4 bgc-positive br-lg f" style="--fg: 1;">
             <div class="alert alert-alt-primary">
                 Give importance with this simple alert.
             </div>
@@ -47,6 +47,31 @@
                 Warning. Loading of this page taking a way too long.
             </div>
             <div class="alert alert-alt-error mb-0">
+                <span class="ico" style="--ico-image: url(/icons/alert-octagon.svg);">&nbsp;</span>
+                Error. Can`t connect to the platform.
+            </div>
+        </div>
+        <div class="d-grid p-4 g-4 bgc-positive br-lg f" style="--fg: 1;">
+            <div class="alert alert-outline-primary">
+                Give importance with this simple alert.
+            </div>
+            <div class="alert alert-outline-secondary">
+                Secondary alerts are likely to be rarely used.
+            </div>
+            <hr class="mw-75 mx-auto">
+            <div class="alert alert-outline-info">
+                <span class="ico" style="--ico-image: url(/icons/info-circle.svg);">&nbsp;</span>
+                All servers are now running smoothly again! Thanks.
+            </div>
+            <div class="alert alert-outline-success">
+                <span class="ico" style="--ico-image: url(/icons/check-circle.svg);">&nbsp;</span>
+                Success! This is positive alert.
+            </div>
+            <div class="alert alert-outline-warning">
+                <span class="ico" style="--ico-image: url(/icons/alert-triangel.svg);">&nbsp;</span>
+                Warning. Loading of this page taking a way too long.
+            </div>
+            <div class="alert alert-outline-error mb-0">
                 <span class="ico" style="--ico-image: url(/icons/alert-octagon.svg);">&nbsp;</span>
                 Error. Can`t connect to the platform.
             </div>
