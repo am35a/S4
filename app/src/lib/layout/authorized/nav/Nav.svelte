80b93bdfff781affeb8e1c1d54e6f99fdf13e3c8
Arthur A. Selimov <am35a.piston@gmail.com>
2023-12-19 22:46:18 +0300

    create tmp segments in nav bar

--- app/src/lib/layout/authorized/nav/Nav.svelte
+++ app/src/lib/layout/authorized/nav/Nav.svelte
@@ -1,19 +1,11 @@
 <script lang="ts">
-
+    import Segments from './segmentslist/Segments.svelte'
 </script>
 
 <nav
     {...$$restProps}
 >
-    <!-- class="p-sticky t-0 d-grid p-2 me-auto" -->
-    <div
-        class="d-grid p-2 bgc-positive br bs-xs ai-start border border-negative-30"
-    >
-        <button class="btn jc-start px-1" type="reset">
-            <span class="ico ico-ghost" style="--ico-image: url(/icons/ghost.svg);">&nbsp;</span>
-            Button
-        </button>
-    </div>
+    <Segments class="d-grid g-2 p-2 bgc-positive br bs-xs ai-start border border-negative-30" />
 </nav>
 
 <style lang="sass">
---
+++ app/src/lib/layout/authorized/nav/segmentslist/Segments.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { path } from 'svelte-micro'
+    import { asideItems, appSegment } from 'src/store/store'
+    // import Item from './item/Item.svelte'
+
+    $: $appSegment = $path //for browser backward and forward buttons
+</script>
+
+<div
+    class:segments = {true}
+    {...$$restProps}
+>
+    {#each $asideItems as item (item.id)}
+        <button
+            id="id-{item.id}"
+            class="btn jc-start px-1"
+            type="reset"
+            disabled={$appSegment === item.segment}
+        >
+            <span class="ico ico-ghost" style="--ico-image: url(/icons/{item.icon}.svg);">&nbsp;</span>
+            <span class="truncate">{item.title}</span>
+        </button>
+
+        <!-- <Item
+            {...item}
+            active={$appSegment === item.segment}
+            segment={item.segment}
+        /> -->
+    {/each}
+    <!-- <button class="btn jc-start px-1" type="reset">
+        <span class="ico ico-ghost" style="--ico-image: url(/icons/ghost.svg);">&nbsp;</span>
+        <span class="truncate">Title of nav menu item with long name</span>
+    </button> -->
+</div>
+
+<style lang="sass">
+</style>
