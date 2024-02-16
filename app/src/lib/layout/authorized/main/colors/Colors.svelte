cbf9a3673384f673c39eedd9519da2c88f8e347f
Arthur A. Selimov <am35a.piston@gmail.com>
2024-02-17 00:28:19 +0300

    fixed colors wars for customizing

--- app/src/lib/layout/authorized/main/colors/Colors.svelte
+++ app/src/lib/layout/authorized/main/colors/Colors.svelte
@@ -57,110 +57,117 @@
 
 <h1 id="colorPalette">Color palette</h1>
 <article>
-    <div>
-        <div>Lightness:</div>
-        <div class="d-grid gtc-3 ai-end px-2">
-            <small class="ta-start">{colorPaletteObj.variablesObj['lightness-offset']}</small>
-            <div class="ta-center">{colorPaletteObj.variablesObj['lightness']}</div>
-            <small class="ta-end">{1 - colorPaletteObj.variablesObj['lightness-offset']}</small>
-        </div>
-        <Input
-            bind:value={colorPaletteObj.variablesObj['lightness']}
-            min={colorPaletteObj.variablesObj['lightness-offset']}
-            max={1 - colorPaletteObj.variablesObj['lightness-offset']}
-            class="frm w-100"
-            type="range"
-            step=".01"
-        />
-        <!-- <div>Lightness offset: {colorPaletteObj.variablesObj['lightness-offset']}</div>
-        <Input
-            bind:value={colorPaletteObj.variablesObj['lightness-offset']}
-            min="0" max="1"
-            class="frm w-100"
-            type="range"
-            step=".01"
-        /> -->
-        <div>Chroma:</div>
-        <div class="d-grid gtc-3 ai-end px-2">
-            <small class="ta-start">0</small>
-            <div class="ta-center">{colorPaletteObj.variablesObj['chroma']}</div>
-            <small class="ta-end">0.37</small>
-        </div>
-        <Input
-            bind:value={colorPaletteObj.variablesObj['chroma']}
-            min="0" max="0.37"
-            class="frm w-100"
-            type="range"
-            step=".01"
-        />
-        <div>Alfa (mute):</div>
-        <div class="d-grid gtc-3 ai-end px-2">
-            <small class="ta-start">0</small>
-            <div class="ta-center">{colorPaletteObj.variablesObj['alfa']}</div>
-            <small class="ta-end">1</small>
-        </div>
-        <Input
-            bind:value={colorPaletteObj.variablesObj['alfa']}
-            min="0" max="1"
-            class="frm w-100"
-            type="range"
-            step=".01"
-        />
-    </div>
-</article>
-<div>
-    Colors
-</div>
-<div class="d-flex fw-wrap g-2">
-    {#each colorPaletteObj.accentNamesArr as value}
-        <div class="d-grid rg-2 ji-center p-4 bgc-positive br f" style="--fg: 1; --fb: 0;">
+    <section>
+        <h2 id="accentColors">Accent</h2>
+        <div>
+            <div>Lightness:</div>
+            <div>
+                <div class="d-grid gtc-3 ai-end px-2">
+                    <small class="ta-start">{colorPaletteObj.variablesObj['lightness-offset']}</small>
+                    <div class="ta-center">{colorPaletteObj.variablesObj['lightness']}</div>
+                    <small class="ta-end">{1 - colorPaletteObj.variablesObj['lightness-offset']}</small>
+                </div>
+                <Input
+                    bind:value={colorPaletteObj.variablesObj['lightness']}
+                    min={colorPaletteObj.variablesObj['lightness-offset']}
+                    max={1 - colorPaletteObj.variablesObj['lightness-offset']}
+                    class="frm w-100"
+                    type="range"
+                    step=".01"
+                />
+            </div>
+            <!-- <div>Lightness offset: {colorPaletteObj.variablesObj['lightness-offset']}</div>
             <Input
-                bind:value={colorPaletteObj.variablesObj[`${value}-hue`]}
-                min="0" max="360"
-                class="frm w-100 bgi"
-                style="{bgiGradientVar()}"
+                bind:value={colorPaletteObj.variablesObj['lightness-offset']}
+                min="0" max="1"
+                class="frm w-100"
                 type="range"
-                step="1"
-            />
+                step=".01"
+            /> -->
+            <div>Chroma:</div>
             <div>
-                <div class="ta-center fw-600 text c-{value}">{value}</div>
-                oklch(
-                    {colorPaletteObj.variablesObj['lightness']}
-                    {colorPaletteObj.variablesObj['chroma']}
-                    {colorPaletteObj.variablesObj[`${value}-hue`]}
-                )
+                <div class="d-grid gtc-3 ai-end px-2">
+                    <small class="ta-start">0</small>
+                    <div class="ta-center">{colorPaletteObj.variablesObj['chroma']}</div>
+                    <small class="ta-end">0.37</small>
+                </div>
+                <Input
+                    bind:value={colorPaletteObj.variablesObj['chroma']}
+                    min="0" max="0.37"
+                    class="frm w-100"
+                    type="range"
+                    step=".01"
+                />
             </div>
-            <div class="d-flex fw-nowrap ji-center">
-                <div class="ws-nowrap as-end">
-                    <div class="ta-center fw-600 c-{value}-l">light</div>
-                    oklch(
-                        {(+(colorPaletteObj.variablesObj['lightness']) + ~~colorPaletteObj.variablesObj['lightness-offset'])}
-                        {colorPaletteObj.variablesObj['chroma']}
-                        {colorPaletteObj.variablesObj[`${value}-hue`]}
-                    )
-                </div>
-                <div>
-                    <ColorBox colorName={value} />
-                </div>
-                <div class="ws-nowrap as-end">
-                    <div class="ta-center fw-600 c-{value}-d">dark</div>
-                    oklch(
-                        {(+(colorPaletteObj.variablesObj['lightness']) - ~~colorPaletteObj.variablesObj['lightness-offset'])}
-                        {colorPaletteObj.variablesObj['chroma']}
-                        {colorPaletteObj.variablesObj[`${value}-hue`]}
-                    )
-                </div>
-            </div>
+            <div>Alfa (mute):</div>
             <div>
-                <div class="ta-center fw-600 c-{value}-m">mute</div>
-                oklch(
-                    {colorPaletteObj.variablesObj['lightness']}
-                    {colorPaletteObj.variablesObj['chroma']}
-                    {colorPaletteObj.variablesObj[`${value}-hue`]}
-                    /
-                    {colorPaletteObj.variablesObj['alfa']}
-                )
+                <div class="d-grid gtc-3 ai-end px-2">
+                    <small class="ta-start">0</small>
+                    <div class="ta-center">{colorPaletteObj.variablesObj['alfa']}</div>
+                    <small class="ta-end">1</small>
+                </div>
+                <Input
+                    bind:value={colorPaletteObj.variablesObj['alfa']}
+                    min="0" max="1"
+                    class="frm w-100"
+                    type="range"
+                    step=".01"
+                />
+            </div>
+            <div>Colors</div>
+            <div class="d-flex fw-wrap g-2">
+                {#each colorPaletteObj.accentNamesArr as value}
+                    <div class="d-grid rg-2 ji-center p-4 bgc-positive br f" style="--fg: 1; --fb: 0;">
+                        <Input
+                            bind:value={colorPaletteObj.variablesObj[`${value}-hue`]}
+                            min="0" max="360"
+                            class="frm w-100 bgi"
+                            style="{bgiGradientVar()}"
+                            type="range"
+                            step="1"
+                        />
+                        <div>
+                            <div class="ta-center fw-600 text c-{value}">{value}</div>
+                            oklch(
+                                {colorPaletteObj.variablesObj['lightness']}
+                                {colorPaletteObj.variablesObj['chroma']}
+                                {colorPaletteObj.variablesObj[`${value}-hue`]}
+                            )
+                        </div>
+                        <div class="d-flex fw-nowrap ji-center">
+                            <div class="ws-nowrap as-end">
+                                <div class="ta-center fw-600 c-{value}-l">light</div>
+                                oklch(
+                                    {(+(colorPaletteObj.variablesObj['lightness']) + ~~colorPaletteObj.variablesObj['lightness-offset'])}
+                                    {colorPaletteObj.variablesObj['chroma']}
+                                    {colorPaletteObj.variablesObj[`${value}-hue`]}
+                                )
+                            </div>
+                            <div>
+                                <ColorBox colorName={value} />
+                            </div>
+                            <div class="ws-nowrap as-end">
+                                <div class="ta-center fw-600 c-{value}-d">dark</div>
+                                oklch(
+                                    {(+(colorPaletteObj.variablesObj['lightness']) - ~~colorPaletteObj.variablesObj['lightness-offset'])}
+                                    {colorPaletteObj.variablesObj['chroma']}
+                                    {colorPaletteObj.variablesObj[`${value}-hue`]}
+                                )
+                            </div>
+                        </div>
+                        <div>
+                            <div class="ta-center fw-600 c-{value}-m">mute</div>
+                            oklch(
+                                {colorPaletteObj.variablesObj['lightness']}
+                                {colorPaletteObj.variablesObj['chroma']}
+                                {colorPaletteObj.variablesObj[`${value}-hue`]}
+                                /
+                                {colorPaletteObj.variablesObj['alfa']}
+                            )
+                        </div>
+                    </div>
+                {/each}
             </div>
         </div>
-    {/each}
-</div>
+    </section>
+</article>
