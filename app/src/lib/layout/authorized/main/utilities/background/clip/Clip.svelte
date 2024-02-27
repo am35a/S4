4841dcbf79f383de511e61577b1fe4f28f43f087
Arthur A. Selimov <am35a.piston@gmail.com>
2024-02-27 07:01:43 +0300

    fixed clip description and added customize container

--- app/src/lib/layout/authorized/main/utilities/background/clip/Clip.svelte
+++ app/src/lib/layout/authorized/main/utilities/background/clip/Clip.svelte
@@ -2,20 +2,35 @@
     import Precode from 'component/Precode.svelte'
 
     let bgClipArr = ['border', 'padding', 'content', 'text']
+    let containerClasses = 'bgr-round'
 </script>
 
 <section>
     <h3 id="clip">Clip</h3>
     <p>
-        The <b>bga-{bgClipArr.join('/')}</b> property sets made the background extends underneath its border box, padding box, content box or text border cliping.
+        The <b>bga-{bgClipArr.join('/')}</b> property sets made the background extends underneath its
+        border box, padding box, content box or text border cliping.
     </p>
     <div class="d-grid g-5">
+        <div class="d-grid g-4">
+            <b>Customize container</b>
+            <input
+                class="frm"
+                bind:value={containerClasses}
+                type="text"
+                placeholder="Type utility classes to customizing the container"
+            >
+            <div class="fs-sm">
+                Play with the <b>bgr-no</b>, <b>bgo-border</b> or other background classes
+                to view different results.
+            </div>
+        </div>
         <div class="d-grid g-4">
             <b>Preview</b>
             <div class="d-grid g-3 p-3 bgc-positive br">
                 {#each bgClipArr as clip}
                     <div
-                        class="p-4 fs-xxxl fw-700 bgi bgc-negative-10 bgc-{clip} bgr-round"
+                        class="p-4 fs-xxxl fw-700 bgi bgc-negative-10 bgc-{clip} {containerClasses}"
                         style="--bgi: url(/images/logo.svg); border: 1em dashed;"
                     >Lorem ipsum dolor sit amet.</div>
                 {/each}
