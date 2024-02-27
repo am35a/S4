b8f90dd53219990f8760f8a14c359d79f6cc984c
Arthur A. Selimov <am35a.piston@gmail.com>
2024-02-27 06:37:01 +0300

    added background clip

--- app/src/lib/layout/authorized/main/utilities/background/Background.svelte
+++ app/src/lib/layout/authorized/main/utilities/background/Background.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
+    import SectionAttachment from './attachment/Attachment.svelte'
+    import SectionClip from './clip/Clip.svelte'
     import SectionColor from './color/Color.svelte'
     import SectionImage from './image/Image.svelte'
     import SectionOrigin from './origin/Origin.svelte'
     import SectionPosition from './position/Position.svelte'
     import SectionRepeat from './repeat/Repeat.svelte'
     import SectionSize from './size/Size.svelte'
-    import SectionAttachment from './attachment/Attachment.svelte'
 </script>
 
 <article>
     <h2 id="background">Background</h2>
     <SectionAttachment />
+    <SectionClip />
     <SectionColor />
     <SectionImage />
     <SectionOrigin />
---
+++ app/src/lib/layout/authorized/main/utilities/background/clip/Clip.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Precode from 'component/Precode.svelte'
+
+    let bgClipArr = ['border', 'padding', 'content', 'text']
+</script>
+
+<section>
+    <h3 id="clip">Clip</h3>
+    <p>
+        The <b>bga-{bgClipArr.join('/')}</b> property sets made the background extends underneath its border box, padding box, content box or text border cliping.
+    </p>
+    <div class="d-grid g-5">
+        <div class="d-grid g-4">
+            <b>Preview</b>
+            <div class="d-grid g-3 p-3 bgc-positive br">
+                {#each bgClipArr as clip}
+                    <div
+                        class="p-4 fs-xxxl fw-700 bgi bgc-negative-10 bgc-{clip} bgr-round"
+                        style="--bgi: url(/images/logo.svg); border: 1em dashed;"
+                    >Lorem ipsum dolor sit amet.</div>
+                {/each}
+            </div>
+        </div>
+        <div class="d-grid g-4">
+            <b>HTML</b>
+            <Precode>
+                {#each bgClipArr as clip}
+                    {
+`<div class="bgi bgc-${clip}" style="--bgi: url(...)">
+    // ...
+</div>
+`
+                    }
+                {/each}
+            </Precode>
+        </div>
+    </div>
+</section>
