6c39cbe43f6f4a107e9cd212675e561f472f1438
Arthur A. Selimov <am35a.piston@gmail.com>
2024-02-28 18:36:58 +0300

    background repeat fixed

--- app/src/lib/layout/authorized/main/utilities/background/repeat/Repeat.svelte
+++ app/src/lib/layout/authorized/main/utilities/background/repeat/Repeat.svelte
@@ -1,33 +1,56 @@
-<script lang="ts">
+<script>
     import Precode from 'component/Precode.svelte'
-    import Details from 'component/Details.svelte'
+
+    let bgRepeatObj = {
+            'default': '',
+            'repeat-x': '-x',
+            'repeat-y': '-y',
+            'space': '-space',
+            'round': '-round',
+            'no-repeat': '-no'
+        },
+        bgRepeat = 'default'
 </script>
 
 <section>
     <h3 id="backgroundRepeat">Repeat</h3>
     <p>
-        Add the <b>bgr-...</b> classes to container to work with background-position.
+        Add the <b>bgr{Object.values(bgRepeatObj).slice(1).join('/')}</b> classes to container to work with background-position.
     </p>
-    <div class="d-grid g-3 mb-4 p-3 gtc-4 bgc-positive br">
-        <div class="bgi bgc-negative-10 bgr-x p-8" style="--bgi: url(http://placekitten.com/32/32)"></div>
-        <div class="bgi bgc-negative-10 bgr-y p-8" style="--bgi: url(http://placekitten.com/32/32)"></div>
-        <div class="bgi bgc-negative-10 bgr-space p-8" style="--bgi: url(http://placekitten.com/32/32)"></div>
-        <div class="bgi bgc-negative-10 bgr-round p-8" style="--bgi: url(http://placekitten.com/28/28)"></div>
-    </div>
-    <Details>
-        <svelte:fragment slot="title">
-            Code example
-        </svelte:fragment>
-        <svelte:fragment slot="body">
-            <Precode class="br-top-0">
+    <div class="d-grid g-5">
+        <div class="d-grid g-4">
+            <b>Sizes</b>
+            <div class="d-flex fw-wrap g-3 ai-center">
+                {#each Object.entries(bgRepeatObj) as [key, value]}
+                    <button
+                        on:click={() => bgRepeat = key}
+                        class="btn {value}"
+                        class:active = {bgRepeat === key}
+                        type="button"
+                        disabled = {bgRepeat === key}
+                    >{key}</button>
+                {/each}
+            </div>
+        </div>
+        <div class="d-grid g-4">
+            <b>Preview</b>
+            <div class="d-grid g-3 p-3 bgc-positive br">
+                <div
+                    class="bgi bgc-negative-10 bgr{bgRepeatObj[bgRepeat]} bgs ar-16_9"
+                    style="--bgi: url(/images/logo.svg); --bgs: 10em auto"
+                ></div>
+            </div>
+        </div>
+        <div class="d-grid g-4">
+            <b>HTML</b>
+            <Precode>
                 {
-`<div class="bgi bgc-negative-10 bgr-x" style="--bgi: url(http://placekitten.com/32/32)"></div>
-<div class="bgi bgc-negative-10 bgr-y" style="--bgi: url(http://placekitten.com/32/32)"></div>
-<div class="bgi bgc-negative-10 bgr-space" style="--bgi: url(http://placekitten.com/32/32)"></div>
-<div class="bgi bgc-negative-10 bgr-round" style="--bgi: url(http://placekitten.com/28/28)"></div>
+`<div class="bgi bgr${bgRepeatObj[bgRepeat]}" style="--bgi: url(...)">
+    // ...
+</div>
 `
                 }
             </Precode>
-        </svelte:fragment>
-    </Details>
+        </div>
+    </div>
 </section>
