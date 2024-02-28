d7e47ff7eab79d2e287a8e4e0c98a65714e1c73e
Arthur A. Selimov <am35a.piston@gmail.com>
2024-02-28 16:29:50 +0300

    added position of background

--- app/src/lib/layout/authorized/main/utilities/background/image/Image.svelte
+++ app/src/lib/layout/authorized/main/utilities/background/image/Image.svelte
@@ -1,6 +1,5 @@
-<script lang="ts">
+<script>
     import Precode from 'component/Precode.svelte'
-    import Details from 'component/Details.svelte'
 </script>
 
 <section>
@@ -9,22 +8,23 @@
         Add the <b>bgi</b> class to container before work with background-image and use the <b>--bgi</b> variable to control the property.
     </p>
     <div class="d-grid g-5">
-        <div class="d-grid g-5">
-            <div class="d-grid g-4">
-                <b>Preview</b>
-                <div class="d-grid g-3 p-2 bgc-positive br">
-                    <div class="bgi p-8" style="--bgi: url(/images/logo.svg)"></div>
-                </div>
+        <div class="alert alert-info js-start"><span class="ico" style="--ico-image: url(/icons/info-circle.svg);"></span>
+            Pay attentions! The <b>--bgi</b> variable is adaptive. The list of prefixes available on the <a href="/core#map">Core</a> page.
+        </div>
+        <div class="d-grid g-4">
+            <b>Preview</b>
+            <div class="d-grid g-3 p-2 bgc-positive br">
+                <div class="bgi p-8" style="--bgi: url(/images/logo.svg)"></div>
             </div>
-            <div class="d-grid g-4">
-                <b>HTML</b>
-                <Precode>
-                    {
+        </div>
+        <div class="d-grid g-4">
+            <b>HTML</b>
+            <Precode>
+                {
 `<div class="bgi" style="--bgi: url(...)"></div>
 `
-                    }
-                </Precode>
-            </div>
+                }
+            </Precode>
         </div>
     </div>
 </section>
--- app/src/lib/layout/authorized/main/utilities/background/position/Position.svelte
+++ app/src/lib/layout/authorized/main/utilities/background/position/Position.svelte
@@ -1,33 +1,41 @@
-<script lang="ts">
+<script>
     import Precode from 'component/Precode.svelte'
-    import Details from 'component/Details.svelte'
 
-    let bgPositionsArr: string[] = [ 'top-left', 'top', 'top-right', 'left', 'center', 'righ','bottom-left', 'bottom', 'bottom-right' ]
+    let bgPositionsArr = [ 'top-left', 'top', 'top-right', 'left', 'center', 'righ','bottom-left', 'bottom', 'bottom-right' ]
 </script>
 
 <section>
     <h3 id="backgroundPosition">Position</h3>
     <p>
-        Add the <b>bgp-...</b> classes to container to work with background-position.
+        Add the <b>bgp-{bgPositionsArr.join('/')}</b> classes to container to work with background-position.
     </p>
-    <div class="d-grid g-3 mb-4 p-3 gtc-3 bgc-positive br">
-        {#each bgPositionsArr as position }
-            <div class="bgi bgc-negative-10 bgp-{position} p-8" style="--bgi: url(http://placekitten.com/32/32)"></div>
-        {/each}
-    </div>
-    <Details>
-        <svelte:fragment slot="title">
-            Code example
-        </svelte:fragment>
-        <svelte:fragment slot="body">
-            <Precode class="br-top-0">
+    <div class="d-grid g-5">
+        <div class="alert alert-info js-start">
+            <span class="ico" style="--ico-image: url(/icons/info-circle.svg);"></span>
+            Pay attentions! The <b>--bgi</b> variable is adaptive. The list of prefixes available on the <a href="/core#map">Core</a> page.
+        </div>
+        <div class="d-grid g-4">
+            <b>Preview</b>
+            <div class="d-grid gtc-3 g-3 p-2 bgc-positive br">
+                {#each bgPositionsArr as position }
+                    <div
+                        class="bgi bgc-negative-10 bgp-{position} bgs h"
+                        style="--bgi: url(/images/logo.svg); --bgs: 4em auto; --h: 6em"
+                    ></div>
+                {/each}
+            </div>
+        </div>
+        <div class="d-grid g-4">
+            <b>HTML</b>
+            <Precode>
                 {#each bgPositionsArr as position }
                     {
-`<div class="bgi bgp-${position}" style="--bgi: url(http://placekitten.com/32/32)"></div>
+`<div class="bgi bgp-${position}" style="--bgi: url(...)"></div>
 `
                     }
                 {/each}
             </Precode>
-        </svelte:fragment>
-    </Details>
+        </div>
+    </div>
+
 </section>
