bece19d0ce9432f16d731f977980d3bb9ba1b9ff
Arthur A. Selimov <am35a.piston@gmail.com>
2024-03-08 23:32:09 +0300

    fixed AnchorNavigation component for alignment utilities

--- app/src/lib/layout/authorized/main/utilities/alignment/Alignment.svelte
+++ app/src/lib/layout/authorized/main/utilities/alignment/Alignment.svelte
@@ -7,15 +7,7 @@
     import SectionAlignItems from './alignitems/AlignItems.svelte'
     import SectionAlignSelf from './alignself/AlignSelf.svelte'
 
-    let alignmentsObj={
-        verticalAlignment: 'Vertical alignment',
-        justifyContent: 'Justify content',
-        justifyItems: 'Justify items',
-        justifySelf: 'Justify self',
-        alignContent: 'Align content',
-        alignItems: 'Align items',
-        alignSelf: 'Align self'
-    }
+    import AnchorNavigation from 'src/lib/layout/components/AnchorNavigation.svelte'
 </script>
 
 <article>
@@ -27,20 +19,18 @@
     <SectionAlignContent />
     <SectionAlignItems />
     <SectionAlignSelf />
+
     <div class="p-sticky b-0 p-2">
-        <div class="d-flex g-1 background-color--positive p-2 w-fit ms-auto br-md bs-sm border border-negative-30 fs-sm">
-            <div class="d-flex fw-wrap g-1 w-fit ms-auto fs-sm">
-                {#each Object.entries(alignmentsObj) as [key, value]}
-                    <button
-                        class="btn btn-primary btn-alt"
-                        on:click={() => window.open(`#${key}`, '_self')}
-                    >{value}</button>
-                {/each}
-            </div>
-            <button
-                class="btn btn-ico fs-sm"
-                on:click={() => window.open('#alignment', '_self')}
-            ><span class="ico" style="--ico-image: url(/icons/arrow-top-big.svg);"></span></button>
-        </div>
+        <AnchorNavigation
+            anchorsObj={{
+                verticalAlignment: 'Vertical alignment',
+                justifyContent: 'Justify content',
+                justifyItems: 'Justify items',
+                justifySelf: 'Justify self',
+                alignContent: 'Align content',
+                alignItems: 'Align items',
+                alignSelf: 'Align self'
+            }}
+        />
     </div>
 </article>
