de124480285987ab62b404b1f526ca3d611b5a4b
Arthur A. Selimov <am35a.piston@gmail.com>
2024-03-14 09:06:02 +0300

    fixed radius description

--- app/src/lib/layout/authorized/main/utilities/border/color/Color.svelte
+++ app/src/lib/layout/authorized/main/utilities/border/color/Color.svelte
@@ -16,35 +16,40 @@
         <b>border-color--...-l</b> and <b>border-color--...-d</b> classes set <b>mute (-m)</b>, <b>light (-l)</b>
         or <b>dark (-d)</b> shades for accent colors of border.
     </p>
-    <div class="d-grid g-3 gtc-2 mb-4 p-3 background-color--positive border-radius--md">
-        <div class="d-flex fd-column-reverse g-2 p-2 background-color--negative">
-            {#each {length: 9} as _, i}
-                <div class="border-style--solid border-color--{defaultColorsArr[0]}-{(i + 1) * 10} p-4 ta-center c-positive">
-                    border-color--{defaultColorsArr[0]}-{(i + 1) * 10}
+    <div class="d-grid g-5">
+        <div class="d-grid g-4">
+            <b>Preview</b>
+            <div class="d-grid g-3 gtc-2 mb-4 p-3 background-color--positive border-radius--md">
+                <div class="d-flex fd-column-reverse g-2 p-2 background-color--negative">
+                    {#each {length: 9} as _, i}
+                        <div class="border-style--solid border-color--{defaultColorsArr[0]}-{(i + 1) * 10} p-4 ta-center c-positive">
+                            border-color--{defaultColorsArr[0]}-{(i + 1) * 10}
+                        </div>
+                    {/each}
+                    <div class="border-style--solid border-color--{defaultColorsArr[0]} p-4 ta-center c-positive">
+                        border-color--{defaultColorsArr[0]}
+                    </div>
                 </div>
-            {/each}
-            <div class="border-style--solid border-color--{defaultColorsArr[0]} p-4 ta-center c-positive">
-                border-color--{defaultColorsArr[0]}
-            </div>
-        </div>
-        <div class="d-flex fd-column-reverse g-2 p-2">
-            {#each {length: 9} as _, i}
-                <div class="border-style--solid border-color--{defaultColorsArr[1]}-{(i + 1) * 10} p-4 ta-center">
-                    border-color--{defaultColorsArr[1]}-{(i + 1) * 10}
+                <div class="d-flex fd-column-reverse g-2 p-2">
+                    {#each {length: 9} as _, i}
+                        <div class="border-style--solid border-color--{defaultColorsArr[1]}-{(i + 1) * 10} p-4 ta-center">
+                            border-color--{defaultColorsArr[1]}-{(i + 1) * 10}
+                        </div>
+                    {/each}
+                    <div class="border-style--solid border-color--{defaultColorsArr[1]} p-4 ta-center">
+                        border-color--{defaultColorsArr[1]}
+                    </div>
                 </div>
-            {/each}
-            <div class="border-style--solid border-color--{defaultColorsArr[1]} p-4 ta-center">
-                border-color--{defaultColorsArr[1]}
-            </div>
-        </div>
-        {#each accentColorsArr as item }
-            <div class="d-flex fd-column-reverse g-2 p-2">
-                {#each Object.entries(accentExtraColorsObj) as [key, value]}
-                    <div class="border-style--solid border-color--{item}{value} p-4 ta-center">
-                        border-color--{item}{value}
+                {#each accentColorsArr as item }
+                    <div class="d-flex fd-column-reverse g-2 p-2">
+                        {#each Object.entries(accentExtraColorsObj) as [key, value]}
+                            <div class="border-style--solid border-color--{item}{value} p-4 ta-center">
+                                border-color--{item}{value}
+                            </div>
+                        {/each}
                     </div>
                 {/each}
             </div>
-        {/each}
+        </div>
     </div>
 </section>
--- app/src/lib/layout/authorized/main/utilities/border/radius/Radius.svelte
+++ app/src/lib/layout/authorized/main/utilities/border/radius/Radius.svelte
@@ -1,10 +1,92 @@
-<script lang="ts">
-    import Add from './add/Radius.svelte'
-    import Cut from './cut/Radius.svelte'
+<script>
+    import Precode from 'component/Precode.svelte'
+
+    let radiusSizesArr = ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl', 'xxxl'],
+        radiusXtraSizes = ['oval', 'pill', 'none']
 </script>
 
 <section>
-    <h3 id="borderRadius">Radius</h3>
-    <Add />
-    <Cut />
+    <h3 id="radius">Radius</h3>
+    <p>
+        To add the default radius use the <b>br</b> class. For different round corners use the <b>border-radius--...</b> classes.
+    </p>
+    <div class="d-grid g-5">
+        <div class="d-grid g-4">
+            <b>Preview</b>
+            <div class="d-grid g-3 p-3 background-color--positive border-radius--md">
+                <div class="d-flex fw-wrap g-2">
+                    {#each radiusSizesArr as item }
+                        <div
+                            class="background-color--negative-10 p-2 border-radius--{item} ta-center ar-4_3 w"
+                            style="--w: 6em"
+                        >{item}</div>
+                    {/each}
+                </div>
+            </div>
+            <div class="d-grid g-3 p-3 background-color--positive border-radius--md">
+                <div class="d-flex fw-wrap g-4">
+                    <div>
+                        <div
+                            class="background-color--negative-10 p-2 border-radius--oval ta-center w"
+                            style="--w: 6em"
+                        >oval</div>
+                    </div>
+                    <div>
+                        <div
+                            class="background-color--negative-10 p-2 border-radius--pill ta-center w"
+                            style="--w: 6em"
+                        >pill</div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-grid g-3 p-3 background-color--positive border-radius--md">
+                <div class="d-flex fw-wrap g-4">
+                    <div
+                        class="background-color--negative-10 p-2 border-radius--oval border-top-left-radius--sm ta-center ar-4_3 w"
+                        style="--w: 6em"
+                    >top left</div>
+                    <div
+                        class="background-color--negative-10 p-2 border-radius--pill border-top-right-radius--xl ta-center ar-4_3 w"
+                        style="--w: 6em"
+                    >top right</div>
+                    <div
+                        class="background-color--negative-10 p-2 border-bottom-right-radius--pill ta-center ar-4_3 w"
+                        style="--w: 6em"
+                    >bottom right</div>
+                    <div
+                        class="background-color--negative-10 p-2 border-radius--xxxl border-bottom-left-radius--none ta-center ar-4_3 w"
+                        style="--w: 6em"
+                    >bottom left</div>
+                </div>
+            </div>
+        </div>
+        <div class="d-grid g-4">
+            <b>HTML</b>
+            <Precode>
+                {#each radiusSizesArr as item }
+                    {
+`<div class="border-radius--${item}">${item}</div>
+`
+                    }
+                {/each}
+            </Precode>
+            <Precode>
+                {#each radiusXtraSizes as item }
+                    {
+`<div class="border-radius--${item}">${item}</div>
+`
+                    }
+                {/each}
+            </Precode>
+            <Precode>
+                {
+`<div class="border-radius--oval border-top-left-radius--sm">top left</div>
+<div class="border-radius--pill border-top-right-radius--xl">top right</div>
+<div class="border-bottom-right-radius--pill">right radius</div>
+<div class="border-radius--xxxl border-bottom-left-radius--none">bottom left</div>
+`
+                }
+            </Precode>
+        </div>
+    </div>
 </section>
